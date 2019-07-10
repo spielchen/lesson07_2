3b46ed9b7836ccb8c8f9f91c2025e88e0795ffa4
spielchen <anna.bukavtsova@gmail.com>
2019-07-10 23:10:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let start = document.querySelector("#start"),
+		plus = document.getElementsByTagName("button")[0],
+		plus1 = document.getElementsByTagName("button")[1],
+		checkbox = document.querySelector("#deposit-check"),
+		income = document.querySelectorAll(".additional_income-item"),
+		rightSide = document.querySelectorAll(".result-total"),
+		salary = document.querySelector(".salary-amount"),
+		incomeTitle = document.querySelector(".income-items"),
+		expenses = document.querySelector(".expenses-items"),
+		additionalExpenses = document.querySelector(".additional_expenses-item"),
+		target = document.querySelector(".target-amount"),
+		period = document.querySelector(".period-select");
+
+
+
+console.log(start);
+console.log(plus, plus1);
+console.log(checkbox);
+console.log(income);
+console.log(rightSide);
+console.log(salary, incomeTitle, expenses, additionalExpenses, target, period);
